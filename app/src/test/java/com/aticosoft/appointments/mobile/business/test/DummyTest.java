8cf5c2a614d32249ec73e67806cf224b838e7d4e
Rodrigo Quesada <rodrigoquesada@gmail.com>
2015-07-25 21:21:36 -0600

    Added initial files/structure/configuration for Android project.

---
+++ app/src/test/java/com/aticosoft/appointments/mobile/business/test/DummyTest.java
@@ -0,0 +1,16 @@
+package com.aticosoft.appointments.mobile.business.test;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Created by rodrigo on 25/07/15.
+ */
+public class DummyTest extends TestSpecification {
+
+    @Test
+    public void test(){
+        fail("Failed!!!");
+    }
+}
